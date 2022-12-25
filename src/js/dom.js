4a5e88bf239721c4f49e0db0fb4b7ab215eabe36
Wantonfury <orzan.alexandru94@gmail.com>
2022-12-25 03:23:30 +0200

    Add some comments

--- src/js/dom.js
+++ src/js/dom.js
@@ -1,4 +1,15 @@
 const domManager = (() => {
+    const addProject = (name, color) => {
+        const list = document.querySelector('#projects-list');
+        
+        list.innerHTML += `
+        <div class="project">
+        <div class="color-picker-color" style="background-color:` + color +`;"></div>
+            <span class="dash-text">` + name + `</span>
+            <span class="dash-tasks"></span>
+        </div>`;
+    }
+    
     const setupHeaderButtons = () => {
         const btnMenu = document.querySelector('#icon-menu');
         const btnHome = document.querySelector('#icon-home');
@@ -18,6 +29,7 @@ const domManager = (() => {
         });
     }
     
+    // Sets up the projects menu in the dashboard
     const setupProjectsTab = () => {
         const add = document.querySelector('#projects-add');
         const overlay = document.querySelector('#projects-add-form-overlay');
@@ -26,6 +38,7 @@ const domManager = (() => {
         
         const toggle = document.querySelector('#projects-toggle');
         
+        // Hide or show project list
         toggle.addEventListener('click', (e) => {
             e.currentTarget.classList.toggle('projects-add-close');
             
@@ -33,19 +46,22 @@ const domManager = (() => {
             list.classList.toggle('projects-list-hide');
         });
         
+        // Show modal for project addition
         add.addEventListener('click', () => {
-            const formContainer = document.querySelector('#projects-add-form-cnt');
+            const formContainer = document.querySelector('#projects-add-modal');
             
             formContainer.classList.remove('form-hide');
         });
         
+        // Exit Project Modal if user clicks outside of it
         overlay.addEventListener('click', (e) => {
             if (e.currentTarget === e.target) {
-                const formContainer = document.querySelector('#projects-add-form-cnt');
+                const formContainer = document.querySelector('#projects-add-modal');
                 formContainer.classList.add('form-hide');
             }
         });
         
+        // Empty name field and reset color choice to default
         const formReset = (formContainer) => {
             const name = document.querySelector('#form-add');
             const colorCode = document.querySelector('#form-color-picker .color-picker-color');
@@ -56,8 +72,9 @@ const domManager = (() => {
             colorName.textContent = 'White';
         }
         
+        // Exit project modal
         btnCancel.addEventListener('click', (e) => {
-            const formContainer = document.querySelector('#projects-add-form-cnt');
+            const formContainer = document.querySelector('#projects-add-modal');
             formContainer.classList.add('form-hide');
             
             formReset(formContainer);
@@ -65,16 +82,23 @@ const domManager = (() => {
             e.preventDefault();
         });
         
+        // Add the project to the list of projects
         form.addEventListener('submit', (e) => {
-            const formContainer = document.querySelector('#projects-add-form-cnt');
+            const formContainer = document.querySelector('#projects-add-modal');
             formContainer.classList.add('form-hide');
             
+            const name = document.querySelector('#form-add').value;
+            const color = document.querySelector('#form-color-picker .color-picker-color').style.backgroundColor;
+            
+            // Check that project name doesn't already exist
+            addProject(name, color);
             formReset(formContainer);
             
             e.preventDefault();
         });
     }
     
+    // Sets the color picker
     const setupProjectsTabColor = () => {
         const colorPicker = document.querySelector('#form-color-picker');
         const list = document.querySelector('#color-picker-list');
@@ -93,10 +117,6 @@ const domManager = (() => {
             } else
                 list.classList.add('hide-color');
         })
-    }
-    
-    const addProject = () => {
-        
     }
     
     const init = () => {
